d10b56ad9960f92e9da38f0d819fa38034eb834b
rf69100 <fonseca.ryan69100@gmail.com>
2026-03-16 17:27:46 +0100

    ajout component register

---
+++ app/components/register.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+// URL de l'API pour poster les données d'inscription
+const API_URL = "https://www.ryanfonseca.fr/b2lp/api/register";
+
+// Composant d'inscription
+export default function Register() {
+  //
+}
